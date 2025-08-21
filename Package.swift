6c2c1b2f432e39d4171406710d1ca3c2caf157c2
shaikdevops786 <shaikdevops786@gmail.com>
2025-08-22 01:47:09 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,12 +9,12 @@ let package = Package(
   ],
   targets: [
     .binaryTarget(
-      name: "OpenSSL",
+      name: "nfcPassportIdSdk",
       url: "https://github.com/shaikdevops786/idv-sdk/releases/download/1.0.0/OpenSSL.xcframework.zip",
       checksum: "16492b581780428fd9dec91df006eb7d47480c0834683ffa7d9cb461f84de165"
     ),
     .binaryTarget(
-      name: "nfcPassportIdSdk",
+      name: "OpenSSL",
       url: "https://github.com/shaikdevops786/idv-sdk/releases/download/1.0.0/nfcPassportIdSdk.xcframework.zip",
       checksum: "5ac6efe3173b3158f7b427df57a427dad8d5fc333cc05f267848fa9a65f41b68"
     ),
